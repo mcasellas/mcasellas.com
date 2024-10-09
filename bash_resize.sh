cfa88f081fa389dea6f76e878d19a41245e05a42
Marc <marc@radiovoltrega.com>
2024-10-09 23:23:52 +0200

    Update bash_resize.sh

--- bash_resize.sh
+++ bash_resize.sh
@@ -1,4 +1,10 @@
-echo "don't -r!"
+for file in images/*; do
+    if [ -f "$file" ]; then  # Check if it's a regular file
+        new_name=$(echo "$file" | tr '-' ' ' | tr '_' ' ')
+        mv "$file" "$new_name"
+    fi
+done
+echo "Files renamed to avoid hyphens and underscores."
 cp images/* images/fulls
 cp images/fulls/* images/thumbs
 echo "images transferred"
