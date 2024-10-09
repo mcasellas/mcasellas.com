f4a587293c6c0835bafe49e12e7d6d335f9949da
Marc <marc@radiovoltrega.com>
2024-10-09 22:54:50 +0200

    Update bash_resize.sh

--- bash_resize.sh
+++ bash_resize.sh
@@ -1,2 +1,9 @@
+echo "don't -r!"
+cp images/* images/fulls
+cp images/fulls/* images/thumbs
+echo "images transferred"
+rm images/*.jpg
+rm images/*.JPG
+rm images/*.png
 for f in images/thumbs/*; do convert "$f" -resize 510 "$f"; done
 echo "thumbs generated"
